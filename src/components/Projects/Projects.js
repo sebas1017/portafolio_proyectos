2a75814b821e973e1facf64037cba448b215a0c1
sebas1017 <henaoerazosebastian995@gmail.com>
2022-04-20 18:13:43 -0500

    agregacion de contenedor de proyecto

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,6 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import rest_countries_api from "./../../Assets/rest_countries_api.jpg"
+import fast_api_crud from "./../../Assets/fast_api_crud.png"
 
 
 function Projects() {
@@ -25,6 +26,20 @@ function Projects() {
               description="Prueba tecnica de desarrollador backend python , donde construi una API sin hacer uso de ningun framework ,se solicito consumir una API externa de paises y posteriormente  procesar la informacion , luego guardar dicha informacion en una base de datos  SQLITE y retornar el JSON de respuesta al cliente en el endpoint raiz del URL , para mas detalles de los requerimientos mediante los cuales desarrolle esta API ver la siguiente imagen"
               link="https://vast-brook-33817.herokuapp.com/"
               link_doc="https://github.com/sebas1017/API-REST-COUNTRIES"
+            />
+          </Col>
+        </Row>
+
+
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={fast_api_crud}
+              isBlog={false}
+              title="USERS API-REST"
+              description="API basada en el marco REST , realiza operaciones CRUD para el registro de usuarios , realizada con FastAPI y Python conectandose a una base de datos PostgreSQL almacenada en heroku"
+              link="https://fastapi-users-rest.herokuapp.com/docs"
+              link_doc="https://github.com/sebas1017/FAST-API-CRUD"
             />
           </Col>
         </Row>
